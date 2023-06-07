479afe745c56f42b4a8a1969c3973de1452a897c
yezhihao <yezhihao@topgamesinc.com>
2023-06-07 11:16:17 +0800

    fix: 小程序不显示准备下降的 block

--- assets/scripts/game.ts
+++ assets/scripts/game.ts
@@ -4,11 +4,14 @@ import {
   AudioSource,
   Component,
   EventTouch,
+  find,
   instantiate,
   Label,
   Node,
   Prefab,
   resources,
+  Sprite,
+  SpriteAtlas,
   tween,
   UITransform,
   Vec3,
@@ -21,8 +24,7 @@ export class Game extends Component {
   @property({ type: [Prefab] })
   blockArr = [];
 
-  @property({ type: Node })
-  showBlockNode: Node;
+  prepareBlockNode: Node;
 
   @property({ type: Node })
   scoreNode: Node;
@@ -41,13 +43,15 @@ export class Game extends Component {
 
   kLimitHeight = 430;
   kWarningHeight = 280;
+  spriteAtlas: SpriteAtlas;
 
   get isGameOver(): boolean {
     return this.gameOverNode.active;
   }
 
   init() {
-    this.showBlockNode.active = false;
+    this.prepareBlockNode = find('Canvas/prepare_block');
+    this.spriteAtlas = this.prepareBlockNode.getComponent(Sprite).spriteAtlas;
     this.prepareBlockSeq = 1;
 
     this.gameOverNode.active = false;
@@ -58,32 +62,23 @@ export class Game extends Component {
     this.cleanupBlocks();
 
     this.showPrepareBlock();
-  }
-
-  showPrepareBlock() {
-    this.showBlockNode.active = true;
-
-    this.showBlockNode.setPosition(0, 530);
-    this.showBlockNode.parent = this.node;
-    const children = this.showBlockNode.children;
-
-    for (let i = 0; i < children.length; i++) {
-      if (children[i].name == `show_fruit_${this.prepareBlockSeq}`) {
-        children[i].active = true;
-      } else {
-        children[i].active = false;
-      }
-    }
-
-    tween(this.showBlockNode)
-      .to(0.1, { scale: new Vec3(0, 0, 0) })
-      .to(0.1, { scale: new Vec3(1, 1, 1) })
-      .delay(0.1)
-      .start();
 
     this.node.on(Node.EventType.TOUCH_START, this.onTouchStart, this);
     this.node.on(Node.EventType.TOUCH_MOVE, this.onTouchMove, this);
     this.node.on(Node.EventType.TOUCH_END, this.onTouchEnd, this);
+  }
+
+  showPrepareBlock() {
+    this.prepareBlockNode.active = true;
+    this.prepareBlockNode.setPosition(0, 530);
+    this.prepareBlockNode.getComponent(Sprite).spriteFrame =
+      this.spriteAtlas.getSpriteFrame('fruit_' + this.prepareBlockSeq);
+
+    tween(this.prepareBlockNode)
+      .to(0.1, { scale: new Vec3(0, 0, 0) })
+      .to(0.1, { scale: new Vec3(1, 1, 1) })
+      .delay(0.1)
+      .start();
   }
 
   start() {
@@ -102,18 +97,18 @@ export class Game extends Component {
     const v3TouchStart = this.node
       .getComponent(UITransform)
       .convertToNodeSpaceAR(new Vec3(v2TouchStart.x, v2TouchStart.y, 0));
-    this.showBlockNode.setPosition(v3TouchStart.x, 530);
+    this.prepareBlockNode.setPosition(v3TouchStart.x, 530);
   }
 
   onTouchMove(event: EventTouch) {
     if (this.isGameOver) return;
-    if (!this.showBlockNode.active) return;
+    if (!this.prepareBlockNode.active) return;
 
     const v2TouchMove = event.getUILocation();
     const v3TouchMove = this.node
       .getComponent(UITransform)
       .convertToNodeSpaceAR(new Vec3(v2TouchMove.x, v2TouchMove.y, 0));
-    this.showBlockNode.setPosition(v3TouchMove.x, 530);
+    this.prepareBlockNode.setPosition(v3TouchMove.x, 530);
   }
 
   createBlock(seq: number, pos: Vec3, canScale: boolean) {
@@ -131,9 +126,9 @@ export class Game extends Component {
 
   onTouchEnd(event: EventTouch) {
     if (this.isGameOver) return;
-    if (!this.showBlockNode.active) return;
-    this.showBlockNode.active = false;
-    const posBlockShow = this.showBlockNode.getPosition();
+    if (!this.prepareBlockNode.active) return;
+    this.prepareBlockNode.active = false;
+    const posBlockShow = this.prepareBlockNode.getPosition();
     this.createBlock(this.prepareBlockSeq, posBlockShow, false);
     this.prepareBlockSeq = Math.floor(Math.random() * 5) + 1;
 
